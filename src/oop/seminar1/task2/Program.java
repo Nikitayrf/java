d4346e553b35f0104b12ccd3e2d06cd4ddf0f11c
«nikita» <samaraphone@yandex.ru>
2025-04-24 17:59:34 +0100

    rename variables in seminar2 oop

--- src/oop/seminar1/task2/Program.java
+++ src/oop/seminar1/task2/Program.java
@@ -2,9 +2,9 @@ package oop.seminar1.task2;
 
 public class Program {
     public static void main(String[] args) {
-        Person p1 = new Person( "Petr" );
-        Cat c1 = new Cat( "Barsic" );
-        System.out.println(p1.sayKiskis( c1 ));
-        System.out.println(p1.sayStop( c1 ));
+        Person person = new Person( "Petr" );
+        Cat cat = new Cat( "Barsic" );
+        System.out.println(person.sayKiskis( cat ));
+        System.out.println(person.sayStop( cat ));
     }
 }
